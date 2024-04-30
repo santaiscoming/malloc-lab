f3efdade787d66277f5a4a487be570cdd8b68ea6
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-04-30 22:52:26 +0900

    chore : macro description

--- mm.c
+++ mm.c
@@ -34,9 +34,52 @@ team_t team = {
     ""};
 
 /*
-  single word (4) or double word (8) alignment
-  @ 8바이트(DOUBLE WORD) 정렬을 위해
+  ------------------- 매크로 설명 -------------------
+  @ void *bp -> 블록포인터 즉, malloc으로 할당받은 블록의 시작주소를 의미
+  @ ALIGNMENT -> 8바이트(DOUBLE WORD) 정렬을 위해
+  @ ALIGN(size) -> size를 8바이트 정렬로 변경
+  @ SIZE_T_SIZE -> size_t의 크기를 8바이트로 변경
+  @ PACK(size, alloc) -> size와 alloc을 하나로 합침 (size : 29bit, alloc : 1bit)
+    ex) size = 0x0000000c, alloc = 0x1 -> 0x0000000d
+
+  @ GET(p) -> p(주소)의 값을 읽어옴
+  @ PUT(p, val) -> p(주소)에 val을 저장
+
+  @ GET_SIZE(p) -> p(주소)의 size를 읽어옴
+    ex) 0x0000000d(할당된 블록) -> 0x0000000c
+        1을 빼준다고 생각하면 됨
+  @ GET_ALLOC(p) -> p(주소)의 할당 여부를 읽어옴
+    ex) GET(p) = 0x0000000d (p의 값이 0x0000000d라면)
+                 0x00000001 (0x1)
+                 FFFFFFFFFT (둘다 1일때 T 즉, T는 1로 생각)
+                 &연산은 둘다 1일때 1을 반환하므로 0x1 반환
+
+  @ HDRP(bp) -> *bp - WSIZE (bp의 주소에서 1WORD(4byte)만큼 뒤로 이동)
+  @ FTRP(bp) -> *bp + GET_SIZE(HDRP(bp)) - DSIZE(footer 크기)
+                (포인터 연산 : bp의 주소에서 size만큼 뒤로 이동)
+      +-----------------+-----------------+-----------------+-----------------+
+      |  Header (WSIZE) |  Payload        |  Padding        |  Footer (WSIZE) |
+      +-----------------+-----------------+-----------------+-----------------+
+                        ⌃(bp가 가르키는 위치)
+          즉, Header만큼 더 이동하기떄문에 Footer를 포함한 DSIZE 빼이함
+
+  @ *** 괄호조심 !!! ***
+  @ NEXT_BLKP(bp) -> *bp + GET_SIZE(((char*)bp - WSIZE))
+        bp의 주소에서 WSIZE 만큼 뒤로 움직이면 HEADER를 가르키기때문에 빼줌
+        즉, 현재 블록의 Header로 옮기고 Header의 size를 읽어와서 더해줌
+  @ PREV_BLKP(bp) -> *bp - GET_SIZE(((char*)bp - DSIZE))
+        GET_SIZE(((char *)(bp)-DSIZE) : 이전 블록의 Footer에서 size를 읽어옴
+          DSIZE를 빼주는 이유 bp가 HEADER에서 시작하고 2칸 뒤로가야 FOOTER를
+          가르키기 떄문
+      +-----------------+-----------------+-----------------+-----------------+
+      |   PREV FOOTER   |   NEXT HEADER   |  Padding(option)|  Footer (WSIZE) |
+      +-----------------+-----------------+-----------------+-----------------+
+      ⌃(bp - DSIZE)         <---          ⌃(bp가 가르키는 위치)
+
+
+  -------------------------------------------------
 */
+/* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
@@ -51,7 +94,6 @@ team_t team = {
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
-/* 비트 연산을 통해 size와 alloc을 하나로 합치기 */
 #define PACK(size, alloc) ((size) | (alloc))
 
 /* Read and write a word at address p */
@@ -59,8 +101,8 @@ team_t team = {
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
 
 /* Read the size and allocated fields from address p */
-#define GET_SIZE(p) (GET(p) & ~0x7)  // 뒤의 3자리를 제외한것의 비트연산이 size
-#define GET_ALLOC(p) (GET(p) & 0x1)  // 0x...o1에서 0번째에서 확인 alloc
+#define GET_SIZE(headerPtr) (GET(headerPtr) & ~0x7)
+#define GET_ALLOC(headerPtr) (GET(headerPtr) & 0x1)
 
 /* Given block ptr bp, compute address of its header and footer */
 #define HDRP(bp) ((char *)(bp)-WSIZE)
@@ -70,6 +112,13 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 // --------------------------------------------------------------
+
+// --------------------- global variable ---------------------
+/*
+  @ heap_listp - 힙의 시작점을 가리키는 포인터
+*/
+static char *heap_listp;
+// -----------------------------------------------------------
 
 /*
  * mm_init - initialize the malloc package.
